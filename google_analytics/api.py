c11b3ae8b3b28441dc6481d37d9fd6979484ee75
System Administrator <root@larus.local>
2011-08-10 18:57:44 -0400

    I added a function google_analytics.api.set_custom_variable(...) which will set a Google Analytics custom variable for the next page view.
    If this function is not called, the functionality remains unchanged.  Right now, I'm only planning on supporting the async code.  Someone else can implement ga.js if they feel so inclined.
    
    I still need to add support for tracking session and visitor-level variables accross multiple page views.  You can still set these with set_custom_variable -you'll just need to track them externally.

---
+++ google_analytics/api.py
@@ -0,0 +1,20 @@
+from google_analytics.context_processors import dict_keys, ga_custom_variable
+
+    
+def set_variable(request, slot, key, value, scope):
+    """
+    Call this function in a view to queue a custom variable for the response
+    These are cleared after each pageview.
+    If a variable slot has already been taken, KeyError will be raised
+
+    In the future I will add support for keeping track of session and visitor-level variables.  For now, you will have to keep track of those on your own.
+
+    """
+    custom_variable = ga_custom_variable(key, value, scope)
+    if dict_keys in request.session:
+        dictionary = request.session[dict_keys]
+    else:
+        dictionary = {}
+    if slot in dictionary:
+        raise KeyError('A variable already exists in that slot')
+    dictionary[slot] = custom_variable
